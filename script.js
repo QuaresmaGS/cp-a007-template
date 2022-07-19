ada5314ca3c47bef89b09c8794ca9363694edbff
Gabriel <gabrielssquaresma@gmail.com>
2022-07-18 21:51:52 -0300

    primeira aula de js

---
+++ script.js
@@ -0,0 +1,57 @@
+//Take my breath away
+
+/* Vai no cabeleleiro
+no exteticista
+trabalha o dia inteiro
+vidade artista */
+
+/*let nome = prompt ("Qual o seu nome?")
+console.log(nome);
+let idade = Number (prompt ("qual a sua idade?"));
+console.log(idade);
+
+nome = "Léo"
+console.log(nome);
+idade = 22
+console.log(idade); fa
+let cidade = "Bh-MG"
+console.log(cidade);
+
+console.log("O nome dele é " , nome, "Ele tem " , idade, "e mora em" , cidade);
+
+let tenhopet = false
+const nomeDoPet = null
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof tenhopet)
+console.log(typeof nomeDoPet)*/
+
+/*const nome = String (prompt ("Qual o seu nome?"));
+console.log(nome);
+let idade = Number (prompt ("qual a sua idade?"));
+console.log(idade);
+let humanoOuNão = Boolean ("true")
+console.log(humanoOuNão);
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof humanoOuNão)*/
+
+/*const titulo = "PumpFiction"
+const lancamento = 1994
+let ebom = true
+ 
+console.log(typeof titulo)
+console.log(typeof lancamento)
+console.log(typeof ebom)*/
+
+let a = ("12")
+let b = ("22")
+let n;
+
+c=a
+a=b
+b=c
+
+console.log("A", a)
+console.log("B", b)
